b6e97802db33c69b964455227f9b44f3220e260c
Gonzalo Pincheira A <g.pincheira.a@gmail.com>
2016-06-27 18:18:04 +0000

    task passing all the tests

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,9 +7,15 @@ class HangpersonGame
 
   # def initialize()
   # end
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses, :check_win_or_lose
   
-  def initialize(word)
+  def initialize(word, guesses='', wrong_guesses='')
     @word = word
+    @guesses = guesses
+    @wrong_guesses = wrong_guesses
+    @word_with_guesses = @word.gsub(/\w/,'-')
+    @attempts = 0
+    @check_win_or_lose = :play
   end
 
   def self.get_random_word
@@ -18,5 +24,44 @@ class HangpersonGame
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
+  def guess(character)
+    
+    if(character == '' || character =~ /\W/ || character == nil)
+      raise(ArgumentError, "Should be a character not empty")
+    end
+    
+    @attempts += 1
+    
+    character = character.downcase
+    valid = @word.index(character) != nil && @guesses != character
+    
+    if(valid)
+      @guesses = character
+      
+      @word.each_char.with_index do |char, index|
+        if(char == character)
+          @word_with_guesses[index] = char
+        end
+      end
+      
+      if(@word_with_guesses == @word)
+         @check_win_or_lose = :win
+      end
+      
+      return true
+    else
+      @wrong_guesses = character
+      
+      if(@attempts == 7)
+        @check_win_or_lose = :lose
+      end
+      return false
+    end
+    
+    
+    
+  end
+  
+  
+  
 end
